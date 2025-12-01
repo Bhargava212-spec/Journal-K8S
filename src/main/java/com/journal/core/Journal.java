4ef02ae3ae60dfc5f30380d594a32fb5eb4ea53e
Bhargava Makkena <bhargava_makkena@epam.com>
2025-12-01 17:44:41 +0530

    entity class changes

--- src/main/java/com/journal/core/Journal.java
+++ src/main/java/com/journal/core/Journal.java
@@ -20,7 +20,7 @@ public class Journal {
     @Column(name = "USER_NAME", nullable = false, updatable = false)
     private String username;
 
-    @Column(name = "USER_EVENT",length = 65535)
+    @Column(name = "USER_EVENT",columnDefinition = "LONGTEXT")
     private String event;
 
     @CreationTimestamp
